57b6379f8941b9e077de1682c06bef524cffc603
olgundutkan <olgundutkan@gmail.com>
2014-10-12 14:57:55 +0300

    Added layout and first view

---
+++ app/views/layouts/default.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Larabook</title>
+	<link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+</head>
+<body>
+
+<div class="container">
+	@yield('content')
+</div>
+
+<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css"></script>
+</body>
+</html>
