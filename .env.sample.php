4101c66cd98b3f55f32ee0d9e6e7a86e10358c3c
olgundutkan <olgundutkan@gmail.com>
2015-01-15 11:55:25 +0200

    Added new api keys to environment

--- .env.sample.php
+++ .env.sample.php
@@ -11,4 +11,25 @@ return array(
 	'DB_DATABASE'  => 'database',
 	'DB_USERNAME'  => 'username',
 	'DB_PASSWORD'  => 'password',
+
+	/**
+	*	THIRD-PARTY 
+	**/
+	'MANDRILL_SECRET'  => 'secret',
+	'MAILCHIMP_APIKEY' => 'secret',
+	'MAILCHIMP_USERS_NEWSLETTER_LIST_ID' => 'list_id',
+	'MAILCHIMP_MUSICIANS_NEWSLETTER_LIST_ID' => 'list_id',
+
+	'S3_KEY' => 'key',
+	'S3_SECRET' => 'secret',
+	'S3_REGION' => 'region',
+	'S3_BUCKET' => 'bucket',
+
+	'SOUNDCLOUD_CLIENT_ID' => 'client_id',
+
+	'RECAPTCHA_SITE_KEY'	=> 'site',
+	'RECAPTCHA_SECRET_KEY'	=> 'secret',
+
+	'PAYPAL_CLIENT_ID'	=> 'client_id',
+	'PAYPAL_CLIENT_SECRET'	=> 'secret'
 );
