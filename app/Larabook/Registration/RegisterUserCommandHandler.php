310d0338e5b656a289c5bc80cf5b39dbac1ce5f3
olgundutkan <olgundutkan@gmail.com>
2015-02-23 10:38:39 +0200

    Modified default value privacy

--- app/Larabook/Registration/RegisterUserCommandHandler.php
+++ app/Larabook/Registration/RegisterUserCommandHandler.php
@@ -40,6 +40,8 @@ class RegisterUserCommandHandler implements CommandHandler
         
         $this->repository->save($user);
 
+        dd($command->is_visible_email);
+
         // TODO:: başka bir methoda taşı
         $user->privacy->email      = $command->is_visible_email ? true : false;
         $user->privacy->title      = $command->is_visible_title ? true : false;
