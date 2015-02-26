953399f72f35e8d04529894ce97f1c4660e37108
olgundutkan <olgundutkan@gmail.com>
2015-02-26 18:00:26 +0200

    Added page show view

--- app/views/pages/show.blade.php
+++ app/views/pages/show.blade.php
@@ -5,7 +5,7 @@
     	<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
     		<h1>{{ $page->title }}</h1>
     		<br />
-    		{{ $page->show }}
+    		{{ $page->content }}
     	</div>
     </div>    
 @stop
